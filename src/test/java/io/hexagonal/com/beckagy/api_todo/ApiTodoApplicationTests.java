a64c5308c1ea8f311f7a7313582e36d5c15933ae
Rebeca Aguiar - pcifba <beca.cout@gmail.com>
2024-08-23 18:10:23 -0300

    todo: add modelmapper configuration

---
+++ src/test/java/io/hexagonal/com/beckagy/api_todo/ApiTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hexagonal.com.beckagy.api_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
